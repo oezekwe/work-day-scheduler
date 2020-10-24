0c03546999f51eea40bb292a2aea8dd45818c5f8
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-23 19:28:32 -0500

    Improve schedule format and added a ColorcodeSheet()

--- DateCreator.js
+++ DateCreator.js
@@ -1,2 +1,15 @@
 var currDate= moment().format('dddd, MMMM Do');
+function ColorcodeSheet(){
+    var currH= parseInt(moment().format('hh'));
+    var merid= moment().format('a');
+    if(merid === "pm" && currH!=12){
+        currH+=12;
+    }
+    $('#time-'+currH).toggleClass("present");
+    for(let otherHRs=currH+1; otherHRs<18; otherHRs++){
+        $('#time-'+otherHRs).toggleClass("future");
+    }
+    console.log(moment().format('hh'));
+}
 $("#currentDay").append(currDate);
+ColorcodeSheet();
